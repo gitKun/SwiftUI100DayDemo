a7110d479138bdbe35fae51b89a71846131c8fac
liyakun <1315385515@qq.com>
2020-03-11 21:04:01 +0800

    project 14 begin
    项目 14 旨在融汇前面内容

---
+++ 13-15UIKit/BucketList_14/BucketList_14/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BucketList_14
+//
+//  Created by DR_Kun on 2020/3/11.
+//  Copyright © 2020 kun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
